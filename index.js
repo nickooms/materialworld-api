6863cc917e17963273240ee7fb275b1c75b58607
nickooms <nick.ooms@neanex.com>
2018-03-11 16:15:47 +0100

    moved Sequelize to src/DB

--- index.js
+++ index.js
@@ -1,47 +1,12 @@
 import express from 'express';
-import Sequelize from 'sequelize';
-// import xpath from 'xpath';
-// import { DOMParser } from 'xmldom';
 
 import { a, ul, h1 } from './html';
 import Point from './src/Point';
 import Street from './src/Street';
 import HttpCache from './src/HttpCache';
 import Migrations from './src/Migrations';
-import dbPromise from './src/DB';
+import dbPromise, { Feature } from './src/DB';
 import WFS from './src/WFS';
-// import KML from './src/KML';
-
-// const select = xpath.useNamespaces({ svg: 'http://www.w3.org/2000/svg' });
-
-/* const getAttribute = (node, name) => {
-  const values = Object.values(node.attributes);
-  return values
-    .find(attribute => attribute.name === name)
-    .value;
-}; */
-
-const sequelize = new Sequelize('database', null, null, {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  operatorsAliases: false,
-});
-const OPTIONS = { timestamps: false };
-const Feature = sequelize.define('feature', {
-  id: {
-    type: Sequelize.STRING,
-    primaryKey: true,
-  },
-  geometry: Sequelize.JSON,
-}, OPTIONS);
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  }, (err) => {
-    console.log('Unable to connect to the database:', err);
-  });
 
 const PORT = 3333;
 
@@ -49,11 +14,6 @@ const ROUTE = {
   migrate: 'DB Migration',
   location: 'Location',
 };
-
-/* const log = (data) => {
-  console.dir(data, { colors: true, depth: null });
-  return data;
-}; */
 
 const streetLink = ({ city, street }) =>
   a(`/location/${city}/${street}`, `${street} ${city}`);
@@ -97,8 +57,6 @@ app.get('/location/:city/:street', async (req, res) => {
   const keys = { formattedAddress: FormattedAddress, locationType: LocationType };
   let object = await Street.get(keys);
   if (object) {
-    // const json = await object.toJSON();
-    // log(json);
     res.send(a(`/street/${object.id}/wbn`, `${city} ${street} WBN`));
     return;
   }
@@ -121,8 +79,6 @@ app.get('/location/:city/:street', async (req, res) => {
     lowerLeft,
     upperRight,
   });
-  // const json = await object.toJSON();
-  // log(json);
   res.send(a(`/street/${object.id}/wbn`, `${city} ${street} WBN`));
 });
 
@@ -130,79 +86,13 @@ app.get('/street/:id/wbn', async (req, res) => {
   const { id } = req.params;
   const street = await Street.byId(id);
   const json = await street.toJSON();
-  // console.log(json);
   const { lowerLeft, upperRight } = json;
   const bbox = [lowerLeft.x, lowerLeft.y, upperRight.x, upperRight.y].join(',');
-  // console.log(bbox);
   const layer = 'WBN';
   const wfs = await WFS.getFeature({ bbox, layer });
-  console.log(wfs);
-  // const text = await WMS.getMap({ bbox, layer });
-  // const doc = new DOMParser().parseFromString(text);
-  // const paths = select('//svg:path', doc);
-  /* const middles = paths.map((path) => {
-    const d = getAttribute(path, 'd')
-      .split(/M| L|Z/g)
-      .filter(x => x !== '')
-      .map((point) => {
-        const [x, y] = point.split(' ').map(parseFloat);
-        return { x, y };
-      });
-    const middle = d.reduce((sum, point) => ({
-      x: sum.x + point.x,
-      y: sum.y + point.y,
-    }), {
-      x: 0,
-      y: 0,
-    });
-    return { x: middle.x / d.length, y: middle.y / d.length };
-  }); */
-  // console.log(middles);
-  /* const features = await Promise.all(middles.map((middle) => {
-    return WMS.getFeature({
-      bbox,
-      layer,
-      i: parseInt(middle.x, 10),
-      j: 450 - parseInt(middle.y, 10),
-    });
-  })); */
-  // console.log(features);
-  /* const URL = 'https://geoservices.informatievlaanderen.be/raadpleegdiensten/GRB/wms';
-  const params = {
-    SERVICE: 'WMS',
-    REQUEST: 'GetMap',
-    FORMAT: 'application/vnd.google-earth.kml+xml', // 'image/svg+xml',
-    TRANSPARENT: 'TRUE',
-    STYLES: `GRB_${layer}`,
-    VERSION: '1.3.0',
-    LAYERS: `GRB_${layer}`,
-    WIDTH: 500,
-    HEIGHT: 450,
-    CRS: 'EPSG:31370',
-    BBOX: bbox,
-  };
-  const url = `${URL}?${querystring.stringify(params)}`;
-  // console.log(url);
-  // const xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
-  const text = await HttpCache.fetch(url, true); */
-  // const kml = text.substr(text.indexOf('<Document>'));
-  // const LookAt = /Lookat.*LookAt/g;
-  // kml;
-  // const kml = KML.parse(text);
-  // .replace(LookAt, '$1');
-  // const text = await response.text();
-  // console.log(text);
-  // const map = await getMqp({ layer: 'WBN', bbox });
-  sequelize
-    .sync({ force: true })
-    .then(() => {
-      console.log('It worked!');
-      wfs.features.forEach((feature) => {
-        Feature.create(feature);
-      });
-    }, (err) => {
-      console.log('An error occurred while creating the table:', err);
-    });
+  wfs.features.forEach((feature) => {
+    Feature.create(feature);
+  });
   res.json(wfs);
 });
 
--- src/DB.js
+++ src/DB.js
@@ -1,5 +1,41 @@
+import Sequelize from 'sequelize';
 import sqlite from 'sqlite';
 
-const dbPromise = sqlite.open('./database.sqlite', { Promise });
+const DB_PATH = './database.sqlite';
+
+const OPTIONS = { timestamps: false };
+
+export const sequelize = new Sequelize('database', null, null, {
+  dialect: 'sqlite',
+  storage: './database.sqlite',
+  operatorsAliases: false,
+});
+
+export const Feature = sequelize.define('feature', {
+  id: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+  geometry: Sequelize.JSON,
+  properties: Sequelize.JSON,
+}, OPTIONS);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  }, (err) => {
+    console.log('Unable to connect to the database:', err);
+  });
+
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log('DB Sync OK.');
+  }, (err) => {
+    console.log('An error occurred while creating the table:', err);
+  });
+
+const dbPromise = sqlite.open(DB_PATH, { Promise });
 
 export default dbPromise;
